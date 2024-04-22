cc02d5246714711e650a99fa3c271315d56bf521
ebj17 <126863752+ebj17@users.noreply.github.com>
2024-04-22 13:55:54 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+class PipeSender
+{
+    static void Main(string[] message)
+    {
+        using var server = new NamedPipeServerStream ("BigPipe");
+        server.WaitForConnection();
+
+        using var writer = new StreamWriter(server);
+        writer.WriteLine("message from sender");
+        writer.Flush();
+    }
+}
